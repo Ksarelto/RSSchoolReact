96fedb2009f92cbb77c243fd58a4887384001561
Artsiom Murashko <ksarelto@tut.by>
2021-08-31 21:02:41 +0300

    refactor: change imports

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,78 +1,64 @@
-import { FC, useEffect, useState, ChangeEvent } from 'react';
-import axios from '../../utils/api';
-import { AxiosResponse } from 'axios';
-import  SearchBar  from '../shared/searchBar';
-import  TableHead  from '../shared/tableHead';
-import { Item, Articles, Search } from '../../../public/types';
-import  TableBody  from '../shared/tableBody';
-import { API, InitSearchState } from '../../utils/constants';
+import { FC, useEffect, ChangeEvent } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/configureStore';
+import SearchBar from '../shared/searchBar';
+import TableHead from '../shared/tableHead';
+import { Item } from '../../../public/types';
+import TableBody from '../shared/tableBody';
+import { itemsData, loading } from '../../reducers/APIItemsSlice';
+import { APIAsyncFunction } from '../../utils/api';
+import { setPage, setPageLimit, setTotalPages } from '../../reducers/searchSlice';
 
 const Home: FC = (): JSX.Element => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [searchState, setSearchState] = useState<Search>(InitSearchState);
-  const [totalPages, setTotalPages] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const search = useSelector((state: RootState) => state.search);
+  const items = useSelector((state: RootState) => state.APIItems);
+  const dispatch = useDispatch();
 
-  const fetchAPI = async () => {
-    setLoading(true);
-    try {
-      const response: AxiosResponse<Articles> = await axios.get('/everything', {
-        params: {
-          q: searchState.text,
-          sortBy: searchState.radio,
-          pageSize: searchState.pageLimit,
-          page: searchState.page,
-          apiKey: API,
-        },
-      });
-      const result: Articles = response.data;
-      const { articles } = result;
-      const totalPagesOnPage = result.totalResults / searchState.pageLimit;
-      setItems(articles);
-      setTotalPages(Math.ceil(totalPagesOnPage));
-    } catch (err) {
-      const response = err.response.data;
-      if (response.status === 'error'){
-        alert(response.message)
-      }
-      console.log(err);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   const options = () => {
     const optionsArray = [];
-    for (let i = 0; i < totalPages; i++) {
-      const option = <option key={i} value={i + 1}>{i + 1}</option>;
+    for (let i = 0; i < search.totalPages; i++) {
+      const option = (
+        <option key={i} value={i + 1}>
+          {i + 1}
+        </option>
+      );
       optionsArray.push(option);
     }
     return optionsArray;
   };
 
   useEffect(() => {
-    if (searchState.text === '') {
-      setItems([]);
-      setTotalPages(1);
+    if (search.searchState.text === '') {
+      dispatch(itemsData([]));
+      dispatch(setTotalPages(0));
       return;
     }
-    fetchAPI();
-  }, [searchState]);
+    dispatch(loading(true));
+    dispatch(APIAsyncFunction(search.searchState));
+  }, [search.searchState]);
 
   function changeState(event: ChangeEvent<HTMLSelectElement>): void {
     const input = event.target;
-    setSearchState({ ...searchState, [input.name]: input.value });
+    if (input.name === 'page'){
+      dispatch(setPage(input.value));
+    } else {
+      dispatch(setPageLimit(input.value));
+    }
   }
 
   return (
     <div className="main">
-      <SearchBar getSearchData={setSearchState} />
+      <SearchBar />
       <div className="table">
         <TableHead />
-        {loading
-          ? <div className="lds-ripple"><div></div><div></div></div>
-          : items.map((el: Item): JSX.Element => {
-            return <TableBody item={el} name={searchState.text}/>;
+        {items.loading ? (
+          <div className="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        ) : items.items.map((el: Item): JSX.Element => {
+            return <TableBody key={el.publishedAt} item={el} name={search.searchState.text}/>;
           })}
       </div>
       <div className="pagination">
@@ -82,7 +68,7 @@ const Home: FC = (): JSX.Element => {
         <select
           className="pagination__pages-amount"
           id="amount"
-          value={searchState.pageLimit}
+          value={search.searchState.pageLimit}
           name="pageLimit"
           onChange={changeState}
         >
@@ -96,13 +82,13 @@ const Home: FC = (): JSX.Element => {
         <select
           className="pagination__page-number"
           id="number"
-          value={searchState.page}
+          value={search.searchState.page}
           name="page"
           onChange={changeState}
         >
           {options()};
         </select>
-        <span className="pagination__all-pages">Total pages: {totalPages}</span>
+        <span className="pagination__all-pages">Total pages: {search.totalPages}</span>
       </div>
     </div>
   );
