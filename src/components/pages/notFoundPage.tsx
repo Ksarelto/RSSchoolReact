bfa47c9a08fafab7db9776026374050b3da3ac62
Artsiom Murashko <ksarelto@tut.by>
2021-08-31 13:49:21 +0300

    refactor: add type, refactor code, change export of NotFoundPage component

--- src/components/pages/notFoundPage.tsx
+++ src/components/pages/notFoundPage.tsx
@@ -1,8 +1,15 @@
-import { Link } from "react-router-dom";
-export const NotFoundPage = (): JSX.Element => {
+import { FC } from 'react';
+
+const NotFoundPage: FC = (): JSX.Element => {
   return (
     <div className="notFound">
-      <p className="notFound__text">Sorry, but this page is not exist/ Try to return to <span className="notFound__span">Home</span> or <span className="notFound__span">About</span> pages</p>
+      <p className="notFound__text">
+        Sorry, but this page is not exist/ Try to return to
+        <span className="notFound__span">Home</span> or
+        <span className="notFound__span">About</span> pages
+      </p>
     </div>
   );
 };
+
+export default NotFoundPage;
