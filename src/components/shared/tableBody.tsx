d826545d1211bbab4afd05485e651e82457bef52
Artsiom Murashko <ksarelto@tut.by>
2021-08-31 13:08:41 +0300

    refactor: change import, type, props annotation in TableBody comonent

--- src/components/shared/tableBody.tsx
+++ src/components/shared/tableBody.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Item } from '../../public/types';
-import { ItemDiscription } from '../pages/itemDiscription';
+import { Item } from '../../../public/types';
 
-export const TableBody = (props: { item: Item, name: string }): JSX.Element => {
+interface TableBodyProps {
+  item: Item;
+  name: string;
+}
+
+const TableBody: FC<TableBodyProps> = ({ item, name }): JSX.Element => {
   return (
     <div className="table__body">
-      <NavLink to={`details/${props.name}&&${props.item.publishedAt}`} className="table__body-nav-link"/>
-      <span className="table__body-span table__body-author">{props.item.author}</span>
-      <span className="table__body-span table__body-tittle">{props.item.title}</span>
-      <span className="table__body-span table__body-description">{props.item.description}</span>
+      <NavLink to={`details/${name}&&${item.publishedAt}`} className="table__body-nav-link"/>
+      <span className="table__body-span table__body-author">{item.author}</span>
+      <span className="table__body-span table__body-tittle">{item.title}</span>
+      <span className="table__body-span table__body-description">{item.description}</span>
       <div className="table__body-span table__body-image-wrapper">
-        <img className="table__body-image" src={props.item.urlToImage} alt="image" />
+        <img className="table__body-image" src={item.urlToImage} alt="image" />
       </div>
-      <span className="table__body-span table__body-published">{props.item.publishedAt}</span>
-      <span className="table__body-span table__body-content">{props.item.content}</span>
+      <span className="table__body-span table__body-published">{item.publishedAt}</span>
+      <span className="table__body-span table__body-content">{item.content}</span>
     </div>
   );
 };
+
+export default TableBody;
