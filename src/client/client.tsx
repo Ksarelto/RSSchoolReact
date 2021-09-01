5c4a47aa96bb6b98d193f9b24a774c5e8250d872
Artsiom Murashko <ksarelto@tut.by>
2021-09-01 17:11:16 +0300

    refactor: chnage render to hydtate in clinet.tsx

---
+++ src/client/client.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './app';
+import { store } from './store/configureStore';
+
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
